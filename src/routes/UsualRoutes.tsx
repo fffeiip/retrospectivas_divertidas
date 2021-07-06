2d404e616ed7ed48bc181062f5c8d367b73b12f1
Felipe Escorel <felipe.escorel@gmail.com>
2021-07-06 03:54:40 -0300

    Development (#35)
    * :ghost:

--- src/routes/UsualRoutes.tsx
+++ src/routes/UsualRoutes.tsx
@@ -1,6 +1,7 @@
 import { Switch, Route } from 'react-router-dom';
 import Home from '../pages/Home';
 import Login from '../pages/Login';
+import Cadastro from '../pages/Cadastro';
 import Dinamicas from '../pages/Dinamicas';
 
 function UsualRoutes() {
@@ -8,6 +9,7 @@ function UsualRoutes() {
     <Switch>
         <Route exact path='/' component={Home} />
         <Route path='/login' component={Login} />
+        <Route path='/cadastro' component={Cadastro} />
         <Route path='/dinamicas' component={Dinamicas} />        
     </Switch>
     
